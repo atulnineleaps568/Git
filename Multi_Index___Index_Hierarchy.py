b4c9aa6305a86d35bdb7e0759b9d7eac8e3da184
Atul Sharma <atul.sharma@nineleaps.com>
2023-06-23 10:59:54 +0530

    initial commit

---
+++ Multi_Index___Index_Hierarchy.py
@@ -0,0 +1,18 @@
+import pandas as pd
+import numpy as np
+
+inside = ['Class A', 'Class B', 'Class C', 'Class A', 'Class B', 'Class C']
+outside = ['School 1', 'School 1', 'School 1', 'School 2', 'School 2', 'School 2']
+
+zip(outside, inside)
+multi_index = list(zip(outside, inside))
+
+print(multi_index)
+     
+hier_index = pd.MultiIndex.from_tuples(multi_index)
+print(hier_index) 
+
+np.random.seed(101)
+df = pd.DataFrame(np.random.randint(70, 100, size = (6,2)), index = hier_index, 
+                  columns = ['1st Semester', '2nd Semester'])
+print(df)
